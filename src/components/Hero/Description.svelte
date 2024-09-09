cd02b28eca18fe770a4d1854020e1e151ab037a2
Aggre <hiroyuki.aggre@gmail.com>
2024-09-09 12:16:13 +0900

    tweak

--- src/components/Hero/Description.svelte
+++ src/components/Hero/Description.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  let open = false
+	let open = false
 
-  const onClick = () => {
-    open = !open
-  }
+	const onClick = () => {
+		open = !open
+	}
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <section class="cursor-pointer lg:cursor-auto" on:click={onClick}>
-  <p class={`overflow-hidden text-left lg:text-xl ${open ? '' : 'truncate'}`}>
-    <slot />
-  </p>
+	<p class={`overflow-hidden text-left lg:text-xl ${open ? '' : 'truncate'}`}>
+		<slot />
+	</p>
 </section>
--- src/components/MembershipTitle.svelte
+++ src/components/MembershipTitle.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
-  import { i18nFactory } from '@devprotocol/clubs-core'
+	import { onMount } from 'svelte'
+	import { i18nFactory } from '@devprotocol/clubs-core'
 
-  import { Strings } from '../i18n'
+	import { Strings } from '../i18n'
 
-  export let hasSingleMembership: boolean = false
+	export let hasSingleMembership: boolean = false
 
-  const i18nBase = i18nFactory(Strings)
-  let i18n = i18nBase(['en'])
+	const i18nBase = i18nFactory(Strings)
+	let i18n = i18nBase(['en'])
 
-  onMount(() => {
-    i18n = i18nBase(navigator.languages)
-  })
+	onMount(() => {
+		i18n = i18nBase(navigator.languages)
+	})
 </script>
 
 <h2 class="text-center text-2xl font-bold lg:text-4xl">
-  {i18n(hasSingleMembership ? 'BecomeMember' : 'SelectMembership')}
+	{i18n(hasSingleMembership ? 'BecomeMember' : 'SelectMembership')}
 </h2>
