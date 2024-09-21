00c2d49918e2789d1aaf8348e1ddac2b4f1d5dab
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-09-21 09:32:11 +0900

    Add videos section and improve header functionality

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,8 @@ declare module '*.astro' {
 	import type { AstroComponentFactory } from 'astro/dist/runtime/server'
 	export default InstanceType<AstroComponentFactory>
 }
+
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
