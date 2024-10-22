20ecf07b41095645326928776cd566cfdd5e20a6
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-10-22 22:36:11 +0900

    Enhance skin display with gradient overlay and dynamic image
    Added a gradient overlay for "Skin" tags in Clip.vue, which now dynamically sets the image as a background for these tags. Also included a new image asset and updated Pages.vue to incorporate this new image for skin tags.

--- src/components/Clips/Clip.vue
+++ src/components/Clips/Clip.vue
@@ -18,29 +18,34 @@ const { image, title, description, tag } = defineProps<Props>()
 }
 
 .gradation::before {
-	content: "";
+	content: '';
 	position: absolute;
 	width: 100%;
 	height: 100%;
 	top: 0;
 	left: 0;
-	background: linear-gradient(0deg, #7639EE 25%, transparent);
+	background: linear-gradient(0deg, #7639ee 25%, transparent);
 }
 </style>
 <template>
 	<div
-		class="flex flex-col gap-4 p-2 rounded border border-gray-300 shadow overflow-hidden"
+		class="flex flex-col gap-4 overflow-hidden rounded border border-gray-300 p-2 shadow"
 		:class="{
 			'bg-white': tag === 'Clip' || tag === 'BGM',
 			'gradation bg-cover bg-no-repeat': tag === 'Skin',
 		}"
 		:style="{
-			'backgroundImage': tag === 'Skin' ? `url(${image})` : '',
+			backgroundImage: tag === 'Skin' ? `url(${image})` : '',
 		}"
 	>
 		<div class="relative overflow-hidden rounded">
-			<img v-if="tag !== 'Skin'" class="aspect-square w-[232px] h-[232px]" :src="image" alt="Clip" />
-			<div v-if="tag === 'Skin'" class="w-[232px] h-[232px]" />
+			<img
+				v-if="tag !== 'Skin'"
+				class="aspect-square h-[232px] w-[232px]"
+				:src="image"
+				alt="Clip"
+			/>
+			<div v-if="tag === 'Skin'" class="h-[232px] w-[232px]" />
 			<div
 				class="absolute bottom-1 left-1 rounded px-2 py-1 text-xs text-white"
 				:class="{
@@ -52,7 +57,7 @@ const { image, title, description, tag } = defineProps<Props>()
 				{{ tag }}
 			</div>
 		</div>
-		<div class="flex w-[9.357rem] flex-col gap-1 z-10">
+		<div class="z-10 flex w-[9.357rem] flex-col gap-1">
 			<h3
 				class="overflow-hidden text-ellipsis text-nowrap font-bold"
 				:class="{
