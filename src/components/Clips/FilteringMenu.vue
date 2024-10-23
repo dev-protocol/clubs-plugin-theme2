c9ceafa68d6eccd180cc5dd7e1486d1d8aaa05a3
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-10-23 18:16:22 +0900

    Create FilteringMenu component for clips
    Extracted filtering menu into a dedicated `FilteringMenu.vue` component to improve code modularity and reuse. Updated `Pages.vue` to utilize the new component, ensuring consistent menu functionality and styling across different screen sizes.

---
+++ src/components/Clips/FilteringMenu.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<ul
+		class="flex flex-row gap-4 overflow-x-auto md:flex-col md:overflow-visible"
+	>
+		<li>
+			<button
+				type="button"
+				class="rounded-lg border-gray-800 bg-gray-800 px-8 py-4 text-sm font-bold text-white ring-2 ring-gray-800 ring-offset-2 ring-offset-white hover:bg-gray-900"
+			>
+				All
+			</button>
+		</li>
+		<li>
+			<button
+				type="button"
+				class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
+			>
+				Clip
+				<span
+					class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
+				>78</span
+				>
+			</button>
+		</li>
+		<li>
+			<button
+				type="button"
+				class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
+			>
+				Skin
+				<span
+					class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
+				>12</span
+				>
+			</button>
+		</li>
+		<li>
+			<button
+				type="button"
+				class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
+			>
+				BGM
+				<span
+					class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
+				>6</span
+				>
+			</button>
+		</li>
+		<li>
+			<button
+				type="button"
+				class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
+			>
+				PV
+				<span
+					class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
+				>6</span
+				>
+			</button>
+		</li>
+	</ul>
+</template>
--- src/components/Home/Pages.vue
+++ src/components/Home/Pages.vue
@@ -6,6 +6,7 @@ import skin1 from '../../assets/skins/image01.png'
 import Clip from '../Clips/Clip.vue'
 import { onMounted, ref } from 'vue'
 import Gallery from '../Gallery/Gallery.vue'
+import FilteringMenu from '../Clips/FilteringMenu.vue'
 
 const items = ref<
 	| {
@@ -72,71 +73,14 @@ onMounted(async () => {
 <template>
 	<div class="flex flex-col gap-9 md:flex-row">
 		<!-- filtering menu -->
-		<ul
-			class="flex flex-row gap-4 overflow-x-auto md:flex-col md:overflow-visible"
-		>
-			<li>
-				<button
-					type="button"
-					class="rounded-lg border-gray-800 bg-gray-800 px-8 py-4 text-sm font-bold text-white ring-2 ring-gray-800 ring-offset-2 ring-offset-white hover:bg-gray-900"
-				>
-					All
-				</button>
-			</li>
-			<li>
-				<button
-					type="button"
-					class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
-				>
-					Clip
-					<span
-						class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
-						>78</span
-					>
-				</button>
-			</li>
-			<li>
-				<button
-					type="button"
-					class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
-				>
-					Skin
-					<span
-						class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
-						>12</span
-					>
-				</button>
-			</li>
-			<li>
-				<button
-					type="button"
-					class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
-				>
-					BGM
-					<span
-						class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
-						>6</span
-					>
-				</button>
-			</li>
-			<li>
-				<button
-					type="button"
-					class="flex justify-between gap-1 rounded-lg bg-gray-800 p-4 text-sm font-bold text-white hover:bg-gray-900"
-				>
-					PV
-					<span
-						class="rounded-2xl bg-blue-100 px-2 py-1 text-xs font-bold text-cyan-800"
-						>6</span
-					>
-				</button>
-			</li>
-		</ul>
+		<FilteringMenu class="hidden md:flex" />
+
 		<!-- content -->
-		<div class="flex flex-col gap-16">
+		<div class="flex flex-col gap-9 md:gap-16">
 			<section>
 				<Gallery />
 			</section>
+			<FilteringMenu class="flex md:hidden" />
 			<section
 				class="flex flex-wrap justify-between gap-2 md:justify-start md:gap-4"
 			>
