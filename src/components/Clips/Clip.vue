022c5d8dc4134fd9a25ab12aa17a35bb756aba90
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-10-25 00:29:17 +0900

    Refactor home page and clip component for simplicity
    Replaced the home page implementation with a new Vue component, improving maintainability. Updated the clip component to handle various asset types dynamically and removed redundant code.

--- src/components/Clips/Clip.vue
+++ src/components/Clips/Clip.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
 import { defineProps, ref, useTemplateRef, watchEffect } from 'vue'
 import { FastAverageColor } from 'fast-average-color'
+import { PassportItemAssetType } from '@devprotocol/clubs-plugin-passport'
 
 // Define the types of props
 type Props = {
 	image: string
 	title: string
 	description: string
-	tag: 'Clip' | 'Skin' | 'BGM'
+	tag: PassportItemAssetType
 }
+
+const SKIN = ['css', 'stylesheet-link']
+const CLIP = ['image', 'image-link']
+const BGM = ['bgm', 'bgm-link']
+const VIDEO = ['video', 'video-link']
+
+const getTagName = (tag: PassportItemAssetType) => {
+	if (SKIN.includes(tag)) return 'Skin'
+	if (CLIP.includes(tag)) return 'Clip'
+	if (BGM.includes(tag)) return 'BGM'
+	if (VIDEO.includes(tag)) return 'Video'
+	return 'Unknown'
+}
+
 
 // Define props with types
 const { image, title, description, tag } = defineProps<Props>()
@@ -18,11 +33,13 @@ const color = ref<any>('')
 const imageEle = useTemplateRef('image-element')
 
 watchEffect(async () => {
-	if (tag === 'Skin' && imageEle.value) {
+	if (SKIN.includes(tag) && imageEle.value) {
 		const fac = new FastAverageColor()
 		color.value = await fac
 			.getColorAsync(imageEle.value)
 			.catch((e) => new Error(e))
+
+		console.log(color.value)
 	}
 })
 </script>
@@ -45,11 +62,11 @@ watchEffect(async () => {
 	<div
 		class="flex flex-col gap-4 overflow-hidden rounded border border-gray-300 p-1 shadow md:p-2"
 		:class="{
-			'bg-white': tag === 'Clip' || tag === 'BGM',
-			'gradation bg-cover bg-no-repeat': tag === 'Skin',
+			'bg-white': CLIP.includes(tag) || BGM.includes(tag) || VIDEO.includes(tag),
+			'gradation bg-cover bg-no-repeat': SKIN.includes(tag),
 		}"
 		:style="{
-			backgroundImage: tag === 'Skin' ? `url(${image})` : '',
+			backgroundImage: SKIN.includes(tag) ? `url(${image})` : '',
 		}"
 	>
 		<div class="relative overflow-hidden rounded">
@@ -57,32 +74,33 @@ watchEffect(async () => {
 				ref="image-element"
 				class="aspect-square h-[40.458vw] w-[40.458vw] md:h-[232px] md:w-[232px]"
 				:class="{
-					hidden: tag === 'Skin',
+					hidden: SKIN.includes(tag),
 				}"
 				:src="image"
 				alt="Clip"
 			/>
 			<div
-				v-if="tag === 'Skin'"
+				v-if="SKIN.includes(tag)"
 				class="h-[40.458vw] w-[40.458vw] md:h-[232px] md:w-[232px]"
 			/>
 			<div
 				class="absolute bottom-1 left-1 rounded px-2 py-1 text-xs text-white"
 				:class="{
-					'bg-indigo-600': tag === 'Clip',
-					'bg-fuchsia-500': tag === 'Skin',
-					'bg-orange-500': tag === 'BGM',
+					'bg-indigo-600': CLIP.includes(tag),
+					'bg-fuchsia-500': SKIN.includes(tag),
+					'bg-orange-500': BGM.includes(tag),
+					'bg-yellow-500': VIDEO.includes(tag),
 				}"
 			>
-				{{ tag }}
+				{{ getTagName(tag) }}
 			</div>
 		</div>
 		<div class="z-10 flex w-[9.357rem] flex-col gap-1">
 			<h3
 				class="overflow-hidden text-ellipsis text-nowrap font-bold"
 				:class="{
-					'text-base': tag === 'Clip' || tag === 'BGM',
-					'text-white': tag === 'Skin',
+					'text-base': CLIP.includes(tag) || BGM.includes(tag) || VIDEO.includes(tag),
+					'text-white': SKIN.includes(tag),
 				}"
 			>
 				{{ title }}
@@ -90,8 +108,8 @@ watchEffect(async () => {
 			<p
 				class="overflow-hidden text-ellipsis text-nowrap text-sm"
 				:class="{
-					'text-gray-800': tag === 'Clip' || tag === 'BGM',
-					'text-white': tag === 'Skin',
+					'text-gray-800': CLIP.includes(tag) || BGM.includes(tag) || VIDEO.includes(tag),
+					'text-white': SKIN.includes(tag),
 				}"
 				style="--tw-text-opacity: 0.6"
 				v-html="description"
--- src/components/Home/Pages.vue
+++ src/components/Home/Pages.vue
@@ -1,74 +1,15 @@
 <script setup lang="ts">
-import clips1 from '../../assets/clips/clips01.png'
-import clips2 from '../../assets/clips/clips02.png'
-import clips3 from '../../assets/clips/clips03.png'
-import skin1 from '../../assets/skins/image01.png'
 import Clip from '../Clips/Clip.vue'
-import { onMounted, ref } from 'vue'
+import { defineProps } from 'vue'
 import Gallery from '../Gallery/Gallery.vue'
 import FilteringMenu from '../Clips/FilteringMenu.vue'
+import type { PassportItemData } from '../../types.ts'
 
-const items = ref<
-	| {
-			key: number
-			tag: 'Clip' | 'Skin' | 'BGM'
-			image: string
-			title: string
-			description: string
-	  }[]
-	| []
->([])
-
-// Todo Itemsを実装したらこちらは消す
-const getItems = (nextGroupKey: number, count: number) => {
-	const nextItems: {
-		key: number
-		tag: 'Clip' | 'Skin' | 'BGM'
-		image: string
-		title: string
-		description: string
-	}[] = []
-
-	for (let i = 0; i < count; ++i) {
-		const nextKey = nextGroupKey * count + i
-
-		// ランダムな長さのタイトルを作成する
-		const titleLength = Math.floor(Math.random() * 30) + 1
-		const title = Array.from({ length: titleLength }, () => 'foo is bar.').join(
-			'',
-		)
-
-		// ランダムな長さのdescriptionを作成する
-		const descriptionLength = Math.floor(Math.random() * 10) + 1
-		const description = Array.from(
-			{ length: descriptionLength },
-			() => 'bar',
-		).join('')
-
-		// ランダムなタグを作成する
-		const getRandomInt = (min: number, max: number) => {
-			min = Math.ceil(min)
-			max = Math.floor(max)
-			return Math.floor(Math.random() * (max - min + 1)) + min
-		}
-
-		const random = getRandomInt(0, 2)
-		const tag = random === 0 ? 'Clip' : random === 1 ? 'Skin' : 'BGM'
-
-		nextItems.push({
-			key: nextKey,
-			tag: tag,
-			image: random === 0 ? clips1.src : random === 1 ? skin1.src : clips3.src,
-			title: `PV - ${title}`,
-			description: description,
-		})
-	}
-	return nextItems
+type Props = {
+	passportOfferings: PassportItemData[]
 }
 
-onMounted(async () => {
-	items.value = getItems(0, 10)
-})
+const { passportOfferings } = defineProps<Props>()
 </script>
 <template>
 	<div class="flex flex-col gap-9 md:flex-row">
@@ -85,11 +26,11 @@ onMounted(async () => {
 				class="flex flex-wrap justify-between gap-2 md:justify-start md:gap-4"
 			>
 				<Clip
-					v-for="item in items"
-					:tag="item.tag"
-					:image="item.image"
-					:title="item.title"
-					:description="item.description"
+					v-for="item in passportOfferings"
+					:tag="item.passportItem.itemAssetType"
+					:image="item.imageSrc"
+					:title="item.passportItem.itemAssetType"
+					:description="`${item.price} ${item.currency}`"
 				/>
 			</section>
 		</div>
