9009935471512da13086c7f6449fc5f6cf2527b7
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-11-08 08:09:21 +0900

    Refactor Gallery to use SlideContent component
    Replaced inline slide content with a dedicated SlideContent component to improve code modularity and readability. Removed redundant internationalization code and made slide indicators dynamic based on the number of features.

--- src/components/Gallery/Gallery.vue
+++ src/components/Gallery/Gallery.vue
@@ -2,28 +2,14 @@
 import { Carousel, Slide } from 'vue3-carousel'
 import 'vue3-carousel/dist/carousel.css'
 import { defineProps, ref } from 'vue'
-import { i18nFactory } from '@devprotocol/clubs-core'
 import type { HomeConfig } from '../../types.ts'
+import SlideContent from './SlideContent.vue'
 
 type Props = {
 	features: HomeConfig['features']
 }
 
 const { features } = defineProps<Props>()
-
-function mapFeaturesToTitles(features: HomeConfig['features']) {
-	return features.reduce((acc, feature, index) => {
-		return {
-			...acc,
-			[`title${index}`]: feature.title,
-			[`description${index}`]: feature.description,
-		}
-	}, {})
-}
-
-const i18nSeed = mapFeaturesToTitles(features)
-const i18nBase = i18nFactory(i18nSeed)
-const i18n = i18nBase(navigator.languages)
 
 const currentSlide = ref(0)
 
@@ -51,22 +37,7 @@ const slideTo = (index: number) => {
 		v-model="currentSlide"
 	>
 		<Slide v-for="(feature, index) in features" :key="index">
-			<div
-				class="flex aspect-[5/3] w-full overflow-hidden rounded bg-cover bg-top bg-no-repeat md:aspect-[5/3] md:flex-row md:rounded-2xl md:bg-right"
-				:style="`background-image: url(${feature.image});`"
-			>
-				<div
-					class="flex w-full flex-col items-start justify-end p-6 font-bold text-white"
-					:style="`background-image: linear-gradient(0deg, ${feature.color} 10%, transparent 100%);`"
-				>
-					<p class="text-[8.142vw] md:text-[1.852vw]">
-						{{ i18n(`title${index}`) }}
-					</p>
-					<p class="text-[3.563vw] md:text-[0.810vw]">
-						{{ i18n(`description${index}`) }}
-					</p>
-				</div>
-			</div>
+			<SlideContent :feature="feature" />
 		</Slide>
 	</Carousel>
 
@@ -74,12 +45,9 @@ const slideTo = (index: number) => {
 		<div class="relative inline-flex gap-0">
 			<div
 				class="absolute h-[5px] w-[50px] bg-gray-600 transition-all"
-				:style="`left: calc((100% / 4) * ${currentSlide})`"
+				:style="`left: calc((100% / ${features.length}) * ${currentSlide})`"
 			/>
-			<div class="h-[5px] w-[50px] bg-gray-200" @click="slideTo(0)"></div>
-			<div class="h-[5px] w-[50px] bg-gray-200" @click="slideTo(1)"></div>
-			<div class="h-[5px] w-[50px] bg-gray-200" @click="slideTo(2)"></div>
-			<div class="h-[5px] w-[50px] bg-gray-200" @click="slideTo(3)"></div>
+			<div v-for="index in features.length" class="h-[5px] w-[50px] bg-gray-200" @click="slideTo(index -1)"></div>
 		</div>
 	</div>
 </template>
---
+++ src/components/Gallery/SlideContent.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+import { i18nFactory } from '@devprotocol/clubs-core'
+import type { HomeConfig } from '../../types.ts'
+
+type Props = {
+	feature: HomeConfig['features'][0]
+}
+
+const { feature } = defineProps<Props>()
+
+const i18nBase = i18nFactory({
+	title: feature.title,
+	description: feature.description,
+})
+const i18n = i18nBase(navigator.languages)
+
+</script>
+
+<template>
+	<div
+		class="flex aspect-[5/3] w-full overflow-hidden rounded bg-cover bg-top bg-no-repeat md:aspect-[5/3] md:flex-row md:rounded-2xl md:bg-right"
+		:style="`background-image: url(${feature.image});`"
+	>
+		<div
+			class="flex w-full flex-col items-start justify-end p-6 font-bold text-white"
+			:style="`background-image: linear-gradient(0deg, ${feature.color} 10%, transparent 100%);`"
+		>
+			<p class="text-[8.142vw] md:text-[1.852vw]">
+				{{ i18n('title') }}
+			</p>
+			<p class="text-[3.563vw] md:text-[0.810vw]">
+				{{ i18n('description') }}
+			</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
