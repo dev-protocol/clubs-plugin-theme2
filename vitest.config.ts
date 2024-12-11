8f9a1dce99f987f7576ca04f86fabea9b5270d82
yashdesu <yash@devprotocol.xyz>
2024-12-11 23:05:23 +0530

    chore: add resolutions in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { vitePreprocess } from '@astrojs/svelte'
 
 export default getViteConfig({
+	resolve: {
+		mainFields: ['module', 'import', 'browser', 'main'],
+	},
 	ssr: { noExternal: [/^.*clubs-plugin.*$/] },
 	plugins: [vue(), svelte({ preprocess: [vitePreprocess()] })],
 })
