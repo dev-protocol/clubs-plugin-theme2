6172f99ead333db4a3cfb946d2b4fd3454b56f36
Aggre <hiroyuki.aggre@gmail.com>
2024-12-19 18:34:46 +0900

    patch

--- src/components/Home/Modal.vue
+++ src/components/Home/Modal.vue
@@ -1,41 +1,24 @@
 <script setup lang="ts">
-import { Component as VueComponent } from 'vue'
+import { useTemplateRef, Component as VueComponent, watch } from 'vue'
 
-defineProps<{
+const props = defineProps<{
 	isVisible: boolean
 	modalContent: VueComponent
 	attrs?: { [key: string]: any }
 }>()
+
+const dialog = useTemplateRef('dialog')
+
+watch(props, () => {
+	if (props.isVisible) {
+		dialog.value?.showModal()
+	} else {
+		dialog.value?.close()
+	}
+})
 </script>
 
 <style>
-.modal-container {
-	position: fixed;
-	top: 0;
-	right: 0;
-	bottom: 0;
-	left: 0;
-	display: flex;
-	justify-content: center;
-	align-items: flex-end;
-}
-
-@media (min-width: 1024px) {
-	.modal-container {
-		align-items: center;
-	}
-}
-
-.modal-overlay {
-	position: fixed;
-	top: 0;
-	right: 0;
-	bottom: 0;
-	left: 0;
-	background: rgba(0, 0, 0, 0.6);
-	z-index: -1;
-}
-
 .v-enter-active {
 	transition: transform 600ms cubic-bezier(0.07, 1.28, 0.5, 1);
 }
@@ -52,25 +35,29 @@ defineProps<{
 	transform: translate(0, 0);
 }
 
-html:has(#modal-container[data-active='true']) {
+html:has(dialog[open]) {
 	overflow: hidden;
 }
 </style>
 
 <template>
-	<div
-		id="modal-container"
-		v-show="isVisible"
-		class="modal-container z-50 overflow-y-auto"
-		:data-active="isVisible"
+	<dialog
+		ref="dialog"
+		class="fixed inset-0 flex items-center justify-center overflow-y-auto"
+		:class="{ hidden: !isVisible }"
 	>
-		<div class="modal-overlay" @click.stop="$emit('closeEvent')"></div>
+		<div
+			class="fixed inset-0 bg-black/60"
+			@click.stop="$emit('closeEvent')"
+		></div>
 		<Transition>
-			<component class="m-auto py-4" :is="modalContent" v-bind="attrs">
-				<template #after:description>
-					<slot name="after:description" />
-				</template>
-			</component>
+			<div class="relative m-auto w-full py-4">
+				<component v-show="isVisible" :is="modalContent" v-bind="attrs">
+					<template #after:description>
+						<slot name="after:description" />
+					</template>
+				</component>
+			</div>
 		</Transition>
-	</div>
+	</dialog>
 </template>
